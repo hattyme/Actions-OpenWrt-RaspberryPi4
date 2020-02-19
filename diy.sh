b27dd0d8f1a2e6ffc53f4677146671e63475b86a
YYiiEt <60560292+YYiiEt@users.noreply.github.com>
2020-02-19 21:57:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -27,6 +27,7 @@ sed -i '$a\net.bridge.bridge-nf-call-arptables = 1' package/base-files/files/etc
 rm -rf package/network/config/firewall/files/firewall.config
 cp -f ../firewall.config package/network/config/firewall/files/
 
+#添加wifi
 mkdir -p files/etc/config
 cp -f ../wireless files/etc/config/
 
